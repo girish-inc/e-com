4603c3301b32de28f4aff111056ba30d58247811
girish-inc <73536861+girish-inc@users.noreply.github.com>
2025-06-16 22:58:30 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,12 +1,16 @@
-const mongoose = require("mongoose")
+const mongoose = require('mongoose');
 
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
+};
 
-async function connectDB(){
-    try{
-        await mongoose.connect(process.env.MONGODB_URI)
-    }catch(err){
-        console.log(err)
-    }
-}
-
-module.exports = connectDB
+module.exports = connectDB;
